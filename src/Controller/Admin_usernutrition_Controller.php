72e2c62f473aa4989d5fc201d7816910e7a158d6
iheb <mohamediheb.bendhaou@esprit.tn>
2024-04-23 12:12:49 +0100

    backend + metier

--- src/Controller/Admin_usernutrition_Controller.php
+++ src/Controller/Admin_usernutrition_Controller.php
@@ -22,7 +22,32 @@ class AdminUserNutritionController extends AbstractController
          ]);
     }
 
-    #[Route('/{user}', name: 'admin_user_nutrition_show', methods: ['GET'])]
+    
+
+    #[Route('/average-calories-chart', name: 'admin_average_calories_chart', methods: ['GET'])]
+    public function averageCaloriesChart(): Response
+    {
+        // Retrieve all calorie data from the database
+        $allNutritionData = $this->getDoctrine()
+            ->getRepository(UserNutrition::class)
+            ->findAll();
+    
+        // Calculate the average of all calorie data
+        $totalCalories = 0;
+        $count = count($allNutritionData);
+        foreach ($allNutritionData as $nutritionData) {
+            $totalCalories += $nutritionData->getCalorie();
+        }
+        $averageCalories = $count > 0 ? $totalCalories / $count : 0;
+    
+        // Render the template, passing the average calorie consumption data
+        return $this->render('user_nutrition/chart_admin.html.twig', [
+            'averageCalories' => $averageCalories,
+        ]);
+
+}
+
+#[Route('/{user}', name: 'admin_user_nutrition_show', methods: ['GET'])]
     public function show(UserNutrition $userNutrition): Response
     {
         return $this->render('user_nutrition/show_user.html.twig', [
@@ -31,4 +56,6 @@ class AdminUserNutritionController extends AbstractController
     }
 
 
+
+    
 }
